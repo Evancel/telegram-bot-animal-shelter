5a3b373fb1c20253591406e9d9d40775ba657ec9
guritch <guritch1@mail.ru>
2023-09-07 21:03:25 +0300

    commit 1

--- src/main/java/com/application/animalshelter/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/application/animalshelter/listener/TelegramBotUpdatesListener.java
@@ -3,7 +3,10 @@ package com.application.animalshelter.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,13 +17,38 @@ import java.util.List;
 public class TelegramBotUpdatesListener implements UpdatesListener {
     @Autowired
     private TelegramBot telegramBot;
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+
 
     @PostConstruct
     public void init() {
         telegramBot.setUpdatesListener(this);
     }
     @Override
-    public int process(List<Update> list) {
-        return 0;
+    public int process(List<Update> updates) {
+        updates.forEach(update -> {
+            logger.info("Processing update: {}", update);
+            String updateMessageText = update.message().text();
+
+            if (update.message() == null) {
+                return;
+            }
+
+            long chatId = update.message().chat().id();
+
+            if (update.message() != null && "/start".equals(updateMessageText)) {
+                sendMessage(chatId, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+            }
+        });
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+    private void sendMessage(long chatId, String messageText) {
+        SendMessage message = new SendMessage(chatId, messageText);
+        try {
+            telegramBot.execute(message);
+        } catch (Exception e) {
+            logger.error("Error sending message", e);
+        }
     }
 }
