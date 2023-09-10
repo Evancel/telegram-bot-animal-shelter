0f06d9e781b1f925b9dbc91589639bc01cd764a4
GURitch <guritch21@gmail.com>
2023-09-10 17:50:02 +0300

    доделал обработку сообщений

--- src/main/java/com/application/animalshelter/configuration/TelegramBotConfiguration.java
+++ src/main/java/com/application/animalshelter/configuration/TelegramBotConfiguration.java
@@ -5,19 +5,7 @@ import com.pengrad.telegrambot.model.DeleteMyCommands;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-<<<<<<< HEAD
-@Configuration
-public class TelegramBotConfiguration {
-        @Value("${telegram.bot.token}")
-        private String token;
 
-        @Bean
-        public TelegramBot telegramBot() {
-            TelegramBot bot = new TelegramBot(token);
-            bot.execute(new DeleteMyCommands());
-            return bot;
-    }
-=======
 
 @Configuration
 public class TelegramBotConfiguration {
@@ -31,6 +19,4 @@ public class TelegramBotConfiguration {
         bot.execute(new DeleteMyCommands());
         return bot;
     }
-
->>>>>>> 1-feature_TEST
 }
